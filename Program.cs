b5d18c4433a4fe8151a05147c7a0abe27abee83f
Sourav_Bhattacharya10 <sourav.bhattacharya3@gmail.com>
2023-10-15 16:30:02 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
+
+namespace stringlocalization;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        if (args is { Length: 1 })
+        {
+            CultureInfo.CurrentCulture = CultureInfo.CurrentUICulture = CultureInfo.GetCultureInfo(args[0]);
+        }
+
+        HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+        builder.Services.AddLocalization(options =>
+        {
+            options.ResourcesPath = "Resources";
+        });
+
+        builder.Services.AddTransient<MessageService>();
+
+        using IHost host = builder.Build();
+
+        IServiceProvider services = host.Services;
+
+        MessageService messageService = services.GetRequiredService<MessageService>();
+
+        Console.WriteLine("LocaleMsg is : {0}", messageService.GetGreetingMessage());
+        
+        await host.RunAsync();
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,10 @@
+This is a .NET Console App with .NET 7 as framework version.
+Playing around with string localization and resource file in console app.
+
+To run the console app:
+
+For English (en-US):
+dotnet run
+
+For French (fr-FR):
+dotnet run fr-FR
